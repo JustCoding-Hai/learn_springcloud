d752c5577a24dd4a6624c25766926fe019d2a7ea
ethan huang <ethan.huang@fwd.cn>
2021-01-06 18:18:26 +0800

    初学Spring Cloud Gateway

---
+++ gateway/src/main/java/top/javahai/gateway/DemoApplication.java
@@ -0,0 +1,26 @@
+package top.javahai.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.codec.HttpMessageReader;
+import org.springframework.http.codec.HttpMessageWriter;
+import org.springframework.http.codec.ServerCodecConfigurer;
+
+import java.util.List;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+//	@Bean
+//    RouteLocator routeLocator(RouteLocatorBuilder builder){
+//        return  builder.routes().route("test_route", r -> r.path("/get1").uri("http://httpbin.org")).build();
+//    }
+
+}
